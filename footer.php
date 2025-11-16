95daf8b1150f1a5fff2527783908da204aa88324
Admin Panel <joyrod04@icloud.com>
2025-11-16 01:26:27 -0500

    Creating footer.php file with footer content

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <div class="contact-info">
+        <p>Contact us at: <a href="mailto:info@wellproservices.com">info@wellproservices.com</a></p>
+    </div>
+    <div class="social-media-links">
+        <a href="#">Facebook</a>
+        <a href="#">Twitter</a>
+        <a href="#">Instagram</a>
+    </div>
+    <div class="copyright">
+        <p>&copy; 2025 Wellness Pro Services & Solutions. All rights reserved.</p>
+    </div>
+</footer>
